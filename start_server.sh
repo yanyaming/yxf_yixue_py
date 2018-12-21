987b19df096e3708a6b6a4f188f323a769af3d95
Yanyaming <yanyaming4721@qq.com>
2018-12-21 10:40:05 +0800

    Delete start_server.sh

--- start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#goto project root
-curpath=$(cd "$(dirname "$0")"; pwd)
-cd $curpath
-nohup python3 server.py > server.log 2>&1 &
-cd ..
-echo "server is running in background"
-echo "yixue:port=8002(public)"
-exit 0
